568e541e2a089300f16ccd716d69dbaf3bc3e1f1
Ashish092344 <ashishsingh09234@gmail.com>
2025-12-01 16:33:47 +0530

    fix vite basic ssl plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
-  server: {
-    https: true,
-    host: '10.107.5.182', // Network access ke liye
-    port: 5173
-  }
+  plugins: [
+    react(),
+    basicSsl()   // MUST be inside plugins array
+  ],
 });
